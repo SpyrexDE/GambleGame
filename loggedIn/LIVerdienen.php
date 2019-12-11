cb5ad8e815f9032b8d42a2163f58f28a4242a5e3
SpyrexDE <57133330+SpyrexDE@users.noreply.github.com>
2019-12-11 12:27:15 +0100

    Update LIVerdienen.php

--- loggedIn/LIVerdienen.php
+++ loggedIn/LIVerdienen.php
@@ -26,6 +26,32 @@ if (isset($_SESSION['username'])){
 
       <div class="content">
 
+          <center><input onclick="setCookie(’coins’, getCookie(’coins’)+ 1, -1); document.getElementById('labelCoins').innerHTML = 'Geld: '.getCookie(’coins’);" type="button" class ="btnClicker" id="btnClicker" value="[Klicken]"\></center>
+          
+          <script>
+              function setCookie(cname, cvalue, exdays) {
+                  var d = new Date();
+                  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+                  var expires = "expires="+d.toUTCString();
+                  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+                }
+
+                function getCookie(cname) {
+                  var name = cname + "=";
+                  var ca = document.cookie.split(';');
+                  for(var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == ' ') {
+                      c = c.substring(1);
+                    }
+                    if (c.indexOf(name) == 0) {
+                      return c.substring(name.length, c.length);
+                    }
+                  }
+                  return "";
+                }
+          </script>
+          
           
           
       </div>
