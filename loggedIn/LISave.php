c2b0e7f0147369faf5802d877fd91bf703929e0a
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-01-28 20:10:22 +0100

    Update LISave.php

--- loggedIn/LISave.php
+++ loggedIn/LISave.php
@@ -2,6 +2,10 @@
 session_start();
 $coins = $_COOKIE["coins"];
 $username = $_SESSION["username"];
+
+$coins = stripcslashes($coins);
+$coins = mysqli_real_escape_string($database, $coins);
+
 //Mit Server verbinden und Datenbank auswaehlen
 $database = mysqli_connect("gamblegame.mofagames.eu", "GambleGame", "L7cnyeN9DA@Ywx3");
 mysqli_select_db($database, "GambleDB");
