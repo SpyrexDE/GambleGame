838db782bf5cf1199c2c4e0aba99c462cbaec9ea
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-01-28 20:20:09 +0100

    Update LIStats.php

--- loggedIn/LIStats.php
+++ loggedIn/LIStats.php
@@ -1,7 +1,47 @@
 <?php
 session_start();
 
+$result = $database -> query("SELECT * FROM users ORDER BY count LIMIT 10;") or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
+$topTen = $result->fetch_array();
 
-
-
+if (isset($_SESSION['username'])){
 ?>
+<html>
+<head>
+<link rel="stylesheet" href="../styles.css">
+<link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
+<link rel="shortcut icon" type="image/x-icon" href="img/logo.jpg" />
+<title>__________OEG-2100_____________</title>
+</head>
+    <body>
+
+        <div class="header">
+
+            <img class= "logo" src="../img/logo.jpg" height="100" width="100">
+
+            <a href="LIindex.php">Start</a>
+            <a href="LIVerdienen.php">Verdienen</a>
+            <a class="active" href="LIStats.php">Statistiken</a>
+            <?php echo "<label class='text'>Geld: ".$_COOKIE['coins']."</label>"; ?>
+            <a id="Btn_Save" href="LISave.php">Speichern</a>
+            <a id="Btn_Logout" href="LILogout.php">Logout</a>
+
+        </div>
+
+      <div class="content">
+
+        <div>
+          <h1><u><?php echo "$topTen[0]"; ?></u></h1>
+        <h2 class= "subHeading">Lorem ipsum dolor sit amet,</h2>
+          <p class="text">consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <hr/>
+        </div>
+      </div>
+    </body>
+</html>
+
+<?php
+} else{
+header("location: ../Login.php");
+}
+ ?>
