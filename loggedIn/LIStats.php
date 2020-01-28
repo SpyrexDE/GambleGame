1fb42498e5feb8d12547fdee96135b4646139c4c
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-01-28 20:36:25 +0100

    Update LIStats.php

--- loggedIn/LIStats.php
+++ loggedIn/LIStats.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['username'])){
       <div class="content">
 
         <div>
-          <h1><u><?php echo $topTen[3]; ?></u></h1>
+          <h1><u><?php echo $topTen[3]['coins']; ?></u></h1>
         <h2 class= "subHeading">Lorem ipsum dolor sit amet,</h2>
           <p class="text">consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         <hr/>
