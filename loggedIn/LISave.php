d9c9b5c6b22ff4470884f4efe33f23ba1e972fd6
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-01-28 19:59:14 +0100

    Update LISave.php

--- loggedIn/LISave.php
+++ loggedIn/LISave.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
-
+print("Hallo");
 $coins = $_COOKIE["coins"];
 $username = $_SESSION["username"];
 
-
+print("Hallo");
 $database -> query("UPDATE users SET coins='$coins' WHERE username='$username'") or die ("Fehler Speichern des Kontostandes: ".mysqli_error($database));
-
+print("Hallo");
 $_SESSION['notification'] = ["success", "Erfolgreich gespeichert!"];
-
+print("Hallo");
 header("location: loggedIn/LIindex.php");
-
+print("Hallo");
 ?>
