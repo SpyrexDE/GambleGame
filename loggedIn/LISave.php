f4b06cdafb844e0ae0ac576a091c021060ff43fd
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-01-28 20:00:28 +0100

    Update LISave.php

--- loggedIn/LISave.php
+++ loggedIn/LISave.php
@@ -1,14 +1,13 @@
 <?php
 session_start();
-print("Hallo");
 $coins = $_COOKIE["coins"];
 $username = $_SESSION["username"];
+//Mit Server verbinden und Datenbank auswaehlen
+$database = mysqli_connect("gamblegame.mofagames.eu", "GambleGame", "L7cnyeN9DA@Ywx3");
+mysqli_select_db($database, "GambleDB");
 
-print("Hallo");
+
 $database -> query("UPDATE users SET coins='$coins' WHERE username='$username'") or die ("Fehler Speichern des Kontostandes: ".mysqli_error($database));
-print("Hallo");
 $_SESSION['notification'] = ["success", "Erfolgreich gespeichert!"];
-print("Hallo");
 header("location: loggedIn/LIindex.php");
-print("Hallo");
 ?>
