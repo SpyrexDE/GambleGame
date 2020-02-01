1c54e65ae60fa13bcc9d3470d0c7a90f366d9858
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-02-01 16:43:53 +0100

    Create clickReceiver.php

---
+++ loggedIn/clickReceiver.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$username = $_SESSION["username"];
+$click = $_POST["click"];
+
+//Mit Server verbinden und Datenbank auswaehlen
+$database = mysqli_connect("gamblegame.mofagames.eu", "GambleGame", "L7cnyeN9DA@Ywx3");
+mysqli_select_db($database, "GambleDB");
+
+$result = $database -> query("select * from users where username = '$username'") or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
+$row = $result->fetch_array();
+
+if($click == true){
+
+$clicks = $row['clicks'] + 1;
+$database -> query("UPDATE users SET coins='$clicks' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
+
+}
+?>
