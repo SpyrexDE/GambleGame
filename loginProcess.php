508a05118c5eeea17e96c6a31780960e6e9342b8
Spyrex <malikfk100@gmail.com>
2020-02-01 21:01:06 +0100

    Update loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -29,7 +29,7 @@
               if(file_exists("../img/userIMGS/".$username.".jpg")){
                 $_SESSION["image"] = "../img/userIMGS/".$username.".jpg";
               } else {
-                $_SESSION["image"] = "../img/logo.jpg";
+                $_SESSION["image"] = "../img/userIMGS/".$username.".jpg";
               }
 
               setcookie("coins", $row["coins"]);
