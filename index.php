a30dccabfd4f6e72e656842162727ca83e5cf42d
Spyrex <malikfk100@gmail.com>
2020-02-01 20:16:30 +0100

    .

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ if(isset($_SESSION["username"])){
 <link rel="stylesheet" href="styles.css">
 <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
 <link rel="shortcut icon" type="image/x-icon" href="img/logo.jpg" />
+<script src="./dist/SwupJsPlugin.js"></script>
 <title>__________OEG-2100_____________</title>
 </head>
     <body>
