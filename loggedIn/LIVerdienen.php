bf5f1effe0a043d1b745addd9922f8817cb2058e
Spyrex <malikfk100@gmail.com>
2020-02-01 19:46:20 +0100

    Update LIVerdienen.php

--- loggedIn/LIVerdienen.php
+++ loggedIn/LIVerdienen.php
@@ -118,7 +118,7 @@ if (isset($_SESSION['username'])){
               <h1>Coinflip</h1>
               <p class = "text">Wenn du gewinnst, wird sich dein Einsatz vedoppeln. Wenn nicht, dann ist dein Einsatz weg.</p>
               <form action="coinFlip.php" method="POST">
-                <p class = "text">Einsatz:
+                <p class = "text"><b>Einsatz:<b>
                     <input type="text" class="textbox" value="10" name="einsatz" id="textboxCoinFlip"></input>
                 </p>
                   <input type="submit" class ="btnClicker" id="btnCoinFlip" value="[Flip]"/>
