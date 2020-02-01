4b6146f63f08641b7ac1df676d6ffd5c4488190f
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-02-01 17:33:26 +0100

    Update LIindex.php

--- loggedIn/LIindex.php
+++ loggedIn/LIindex.php
@@ -48,7 +48,7 @@ if (isset($_SESSION['username'])){
           
         <div>
           <h1>GambleGame</h1>
-        <h2 class= "subHeading"><u>Willkommen im Spiel <b><?php echo $_SESSION['username'];?></b>!</u></h2>
+        <h2 class= "subHeading">Willkommen im Spiel <b><?php echo $_SESSION['username'];?></b>!</h2>
           <p class="text">Ich freue mich sehr, dass du dich registrieren konntest und eingeloggt hast. Bei dem Menüpunkt "Verdienen" kannst du dir dein Kapital zusammen klicken, und in Glückspiel investieren. Der Menüpunkt "Statistiken" zeigt dir die zehn besten Spieler des Spiels und deren Kontostand an. Wirst du es auch in diese Liste schaffen?</p>
         <hr/>
            <h2 class= "subHeading">Letzte Updates</h2>
