15bd27afc56df86075ada51f720d86a1629d4cfe
Spyrex <malikfk100@gmail.com>
2020-02-04 15:28:52 +0100

    Update clickReceiver.php

--- loggedIn/clickReceiver.php
+++ loggedIn/clickReceiver.php
@@ -12,6 +12,8 @@ $result = $database -> query("select * from users where username = '$username'")
 $row = $result->fetch_array();
 
 $actualDate = new DateTime();
+$actualDate = $actualDate->format('Y-m-d H:i:s');
+
 $lastUpdateDate = $database -> query("select lastRegister from iplist where IP = '$seineIP'") or die ("Fehler: ".mysqli_error($database));
 $lastUpdateDate = mysqli_fetch_array($lastUpdateDate)[0];
 
@@ -22,6 +24,7 @@ if($lastUpdateDate < $actualDate->modify('-1 day')->format('Y-m-d H:i:s')){
     $coins = $row['coins'] + 1;
     $coins = $row['dailyCoins'] + 1;
     $database -> query("UPDATE users SET coins='$coins' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
+    $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
 
     }
 }else{
