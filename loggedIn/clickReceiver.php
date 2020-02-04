e05446a4e3ebc8f34da765028b903f6f854e8623
Spyrex <malikfk100@gmail.com>
2020-02-04 15:32:39 +0100

    Update clickReceiver.php

--- loggedIn/clickReceiver.php
+++ loggedIn/clickReceiver.php
@@ -17,15 +17,18 @@ $actualDate = $actualDate->format('Y-m-d H:i:s');
 $lastUpdateDate = $database -> query("select lastRegister from iplist where IP = '$seineIP'") or die ("Fehler: ".mysqli_error($database));
 $lastUpdateDate = mysqli_fetch_array($lastUpdateDate)[0];
 
-
 if($lastUpdateDate < $actualDate->modify('-1 day')->format('Y-m-d H:i:s')){
-    if($click == true &&  $row['dailyCoins'] <= 50){
+    $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
+    $database -> query("UPDATE users SET dailyCoins='0' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
+}
+
+
+if( && $row['dailyCoins'] <= 50){
+    if($click == true){
 
     $coins = $row['coins'] + 1;
     $coins = $row['dailyCoins'] + 1;
     $database -> query("UPDATE users SET coins='$coins' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
-    $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
-
     }
 }else{
     die("Du hast bereits 50 mal heite geklickt!");
