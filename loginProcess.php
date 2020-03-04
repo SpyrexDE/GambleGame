eb08467ef5c3ffdbec03b82d506c56e2274d17d5
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-03-04 13:00:45 +0100

    Update loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -10,8 +10,6 @@
           $database = mysqli_connect("gamblegame.mofagames.eu", "GambleGame", "L7cnyeN9DA@Ywx3");
           mysqli_select_db($database, "GambleDB");
 
-         $database -> query("insert into debug (inhalt) values ('Bin drin');") or die ("Fehler: ".mysqli_error($database));
-
         //Anti Mysql injection
         $username = stripcslashes($username);
         $password = stripcslashes($password);
@@ -24,7 +22,6 @@
                       or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
         $row = $result->fetch_array();
 
-        $database -> query("insert into debug (inhalt) values ('Vor der Passwortüberprüfung');") or die ("Fehler: ".mysqli_error($database));
 
         if ($row['username'] == $username && $row['password'] == $password ){
 
@@ -37,7 +34,6 @@
                 $_SESSION["image"] = "../img/logo.jpg";
               }
 
-         $database -> query("insert into debug (inhalt) values ('Cookies setzen...');") or die ("Fehler: ".mysqli_error($database));
 
             setcookie("coins", $row["coins"]);
               setcookie("dailyCoins", $row['dailyCoins']);
@@ -50,11 +46,8 @@
               $actualDate = date('Y-m-d H:i:s', time());
               $lastUpdateDate = $database -> query("select dailyCoins from users where username='$username'") or die ("Fehler: ".mysqli_error($database));
 
-         $database -> query("insert into debug (inhalt) values ('DIE STELLE');") or die ("Fehler: ".mysqli_error($database));
-            
                       $database -> query("insert into debug (inhalt) values ('$actualDate');") or die ("Fehler: ".mysqli_error($database));
                       $database -> query("insert into debug (inhalt) values ('$lastUpdateDate');") or die ("Fehler: ".mysqli_error($database));
-         $database -> query("insert into debug (inhalt) values ('DIE STELLE VORBEI');") or die ("Fehler: ".mysqli_error($database));
               
               if($lastUpdateDate->format('Y-m-d H:i:s') < $actualDate->modify('-3 minute')){
                   $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
