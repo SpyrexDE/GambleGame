5192cd332a270277f8e8de9a095839f47d0468d5
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-03-04 12:15:55 +0100

    Update loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -43,6 +43,7 @@
                 mysqli_select_db($database, "GambleDB");
                 $lastUpdateDate = $database -> query("select lastClick from users where username='$username'") or die ("Fehler: ".mysqli_error($database));
                 $lastUpdateDate = mysqli_fetch_array($lastUpdateDate)[0];
+                 die("Bitte auf den '<-' Pfeil klicken (Wird noch gefixt)");
                 if($lastUpdateDate->format('Y-m-d H:i:s') < $actualDate->modify('-3 minute')->format('Y-m-d H:i:s')){
                   return true;
                 } else{
