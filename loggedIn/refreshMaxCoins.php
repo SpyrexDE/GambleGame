8c42e0a5506b84a970b3b4daf060b11822f662a3
Spyrex <malikfk100@gmail.com>
2020-03-09 19:33:08 +0100

    Update refreshMaxCoins.php

--- loggedIn/refreshMaxCoins.php
+++ loggedIn/refreshMaxCoins.php
@@ -1,5 +1,16 @@
 <?php
 
+//Mit DB verbinden
+$database = mysqli_connect("gamblegame.mofagames.eu", "GambleGame", "L7cnyeN9DA@Ywx3");
+mysqli_select_db($database, "GambleDB");
+
+//Suche nach Nutzer in der Datenbank
+$result = $database -> query("select * from users where username = '$username' and password = '$password'")
+              or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
+$row = $result->fetch_array();
+
+$username = $row['username'];
+
 //Reset MaxCoins
 $actualDate = date('Y-m-d H:i:s', time());
 $lastUpdateDate = $database -> query("select lastClick from users where username='$username'") or die ("Fehler: ".mysqli_error($database));
