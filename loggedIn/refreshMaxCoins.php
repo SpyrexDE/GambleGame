880f37ef4d2bd95ec064285da9f0036025a219fd
Spyrex <malikfk100@gmail.com>
2020-03-09 21:27:53 +0100

    Update refreshMaxCoins.php

--- loggedIn/refreshMaxCoins.php
+++ loggedIn/refreshMaxCoins.php
@@ -10,7 +10,7 @@ $username = $_SESSION["username"];
 
 //Reset MaxCoins
 $actualDate = date('Y-m-d H:i:s', time());
-//$actualMinus3 = $actualDate->modify('-3 minute');
+$actualMinus3 = $actualDate->modify('-3 minute');
 $result = $database -> query("select * from users where username = '$username'")
               or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
 $row = $result->fetch_array();
@@ -18,7 +18,7 @@ $lastUpdateDate = $row['lastClick'];
 
         $database -> query("insert into debug (inhalt) values ('$actualDate');") or die ("Fehler: ".mysqli_error($database));
         $database -> query("insert into debug (inhalt) values ('$lastUpdateDate');") or die ("Fehler: ".mysqli_error($database));
-//        $database -> query("insert into debug (inhalt) values ('$actualMinus3');") or die ("Fehler: ".mysqli_error($database));
+        $database -> query("insert into debug (inhalt) values ('$actualMinus3');") or die ("Fehler: ".mysqli_error($database));
 
 if($lastUpdateDate < $actualDate->modify('-3 minute')){
     $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
