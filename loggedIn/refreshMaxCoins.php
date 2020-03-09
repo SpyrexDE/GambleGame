b471bec7f521d0404f0fb2aa13e80b8329f77bf0
Spyrex <malikfk100@gmail.com>
2020-03-09 19:55:10 +0100

    Update refreshMaxCoins.php

--- loggedIn/refreshMaxCoins.php
+++ loggedIn/refreshMaxCoins.php
@@ -17,6 +17,7 @@ $username = $row['username'];
 $actualDate = date('Y-m-d H:i:s', time());
 //$actualMinus3 = $actualDate->modify('-3 minute');
 $lastUpdateDate = $database -> query("select lastClick from users where username='$username'") or die ("Fehler: ".mysqli_error($database));
+die(str($lastUpdateDate));
         $database -> query("insert into debug (inhalt) values ('$actualDate');") or die ("Fehler: ".mysqli_error($database));
         $database -> query("insert into debug (inhalt) values ('$lastUpdateDate');") or die ("Fehler: ".mysqli_error($database));
 //        $database -> query("insert into debug (inhalt) values ('$actualMinus3');") or die ("Fehler: ".mysqli_error($database));
