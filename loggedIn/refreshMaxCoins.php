4ede87a042b1db7376ec4b5bbe2c05b8656a100e
Spyrex <malikfk100@gmail.com>
2020-03-09 21:25:25 +0100

    Update refreshMaxCoins.php

--- loggedIn/refreshMaxCoins.php
+++ loggedIn/refreshMaxCoins.php
@@ -4,7 +4,7 @@
 $database = mysqli_connect("gamblegame.mofagames.eu", "GambleGame", "L7cnyeN9DA@Ywx3");
 mysqli_select_db($database, "GambleDB");
 
-$_SESSION["username"] = $username;
+$username = $_SESSION["username"];
 
 
 
@@ -15,7 +15,7 @@ $result = $database -> query("select * from users where username = '$username'")
               or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
 $row = $result->fetch_array();
 $lastUpdateDate = $row['lastClick'];
-die($row[0]);
+die($row['lastClick']);
         $database -> query("insert into debug (inhalt) values ('$actualDate');") or die ("Fehler: ".mysqli_error($database));
         $database -> query("insert into debug (inhalt) values ('$lastUpdateDate');") or die ("Fehler: ".mysqli_error($database));
 //        $database -> query("insert into debug (inhalt) values ('$actualMinus3');") or die ("Fehler: ".mysqli_error($database));
