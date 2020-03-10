7daabad7bb79344dc805334a89c9e90296b81a1c
Spyrex <malikfk100@gmail.com>
2020-03-10 10:14:33 +0100

    Update refreshMaxCoins.php

--- loggedIn/refreshMaxCoins.php
+++ loggedIn/refreshMaxCoins.php
@@ -14,7 +14,7 @@ $actualMinus3 = date('Y-m-d H:i:s', strtotime('-3 minutes'));
 $result = $database -> query("select * from users where username = '$username'")
               or die("Fehler beim durchsuchen der Datenbank: ".mysqli_error());
 $row = $result->fetch_array();
-
+$lastUpdateDate = $row['lastClick'];
 
 if($lastUpdateDate < $actualMinus3){
     $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
