d84301211a1c91920ae4f65e0fc2ce65a97a265d
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2020-03-18 00:56:09 +0100

    Update refreshMaxCoins.php

--- loggedIn/refreshMaxCoins.php
+++ loggedIn/refreshMaxCoins.php
@@ -16,7 +16,7 @@ $row = $result->fetch_array();
 $lastUpdateDate = $row['lastClick'];
 
 if($lastUpdateDate < $actualMinus3){
-    $database -> query("UPDATE users SET lastClick=FROM_UNIXTIME('$actualDate') WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
+    $database -> query("UPDATE users SET lastClick='$actualDate' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
     $database -> query("UPDATE users SET dailyCoins='0' WHERE username='$username'") or die ("Fehler beim Senden deines Klicks:".mysqli_error($database));
     setcookie("dailyCoins", 0, time()+3600, "/");
     //Update lastClickCookie
