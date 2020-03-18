00d103967c30180dcf9d8e8ee2dd52330b96467d
Spyrex <malikfk100@gmail.com>
2020-03-18 16:20:30 +0100

    Update changeProcess.php

--- loggedIn/changeProcess.php
+++ loggedIn/changeProcess.php
@@ -14,11 +14,11 @@ if (!empty($_POST['user']) && !empty($_POST['pass'])){
     if(ctype_alnum($username) && strlen( $username) >= 4 && strlen( $username) <= 10 && strlen( $password) >= 4 && strlen( $password) <= 20){
 
       if(is_uploaded_file($image)){
+                  die($image);
           //Check upload
           if(!$image["size"] > 300000 && getimagesize($image["tmp_name"])[0] == 300 && getimagesize($image["tmp_name"])[1] == 300 && exif_imagetype($image["tmp_name"]) != IMAGETYPE_JPEG){
           $newfilename = $username . ".jpg";
           move_uploaded_file($image["tmp_name"], "../img/userIMGS/" . $newfilename);
-          die($image);
           } else{
                 $_SESSION['notification'] = ["error", "Das Jpg muss 300x300 Pixel groß sein."];
                 header("location: LIChangeProfile.php");
