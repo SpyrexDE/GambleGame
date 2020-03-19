26c1334a7b2e8c9e983301215f2feae95d1e47b4
Spyrex <malikfk100@gmail.com>
2020-03-19 13:47:39 +0100

    Update LIindex.php

--- loggedIn/LIindex.php
+++ loggedIn/LIindex.php
@@ -62,7 +62,7 @@ include "LIkicker";
                 <li>Neue Datenbankverbindung</li>
                 <li>Kleinere bugfixes</li>
                 <li>Coinflip hinzugefügt</li>
-                <li><?php echo $_SESSION["lastLogin"];?></li>
+                <li><?php echo time()-$_SESSION["lastLogin"];?></li>
             </ul>
         </div>
       </div>
