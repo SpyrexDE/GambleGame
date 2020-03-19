7e008d48117cbf77f7b0f26c4129fce45a934607
Spyrex <malikfk100@gmail.com>
2020-03-19 13:51:30 +0100

    Update LIkicker.php

--- loggedIn/LIkicker.php
+++ loggedIn/LIkicker.php
@@ -1,13 +1,10 @@
 <?php session_start();
 
-if(time()-$_SESSION["lastLogin"] >= 20){
-  session_start();
-
+if(time()-$_SESSION["lastLogin"] >= 1200){
   session_destroy();
 
   session_start();
   $_SESSION['notification'] = ["warning", "Um das Schummeln von bösen Bot-Meistern auf GambleGame zu verhinden haben wir dich nach 20 Minuten online-Zeit gekickt. Wenn du weiterspielen möchtest, kannst du dich einfach erneut einloggen!"];
-
   header("location: ../Login.php");
 }
 
